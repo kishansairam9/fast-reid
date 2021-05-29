ec81e3e373a911e7398457e606007c91619c970b
kishansairam9 <kishansairam.adapa9@gmail.com>
2021-05-29 19:03:23 +0530

    low scale training

---
+++ run_test.sh
@@ -0,0 +1,1 @@
+pipenv run python3 tools/train_net.py --config-file logs/veriwild/bagtricks_R50-ibn_4gpu/config.yaml --eval-only --num-gpus 1 MODEL.WEIGHTS logs/veriwild/bagtricks_R50-ibn_4gpu/model_best.pth
